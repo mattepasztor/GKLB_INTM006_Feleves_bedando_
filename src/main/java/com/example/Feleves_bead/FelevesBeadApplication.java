b7f1d3b7a2ea8df6fa5848e3e4eb73e6ccecc809
pmate <pasztor.mate91@gmail.com>
2022-12-07 01:49:58 +0100

    First commit with Database setup

---
+++ src/main/java/com/example/Feleves_bead/FelevesBeadApplication.java
@@ -0,0 +1,13 @@
+package com.example.Feleves_bead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelevesBeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelevesBeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Feleves_bead/FelevesBeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Feleves_bead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelevesBeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
